4af37e7e58453884c4fcb36897493e2deb56b91c
forest <10756047@ntub.edu.tw>
2022-09-05 22:44:34 +0800

    註冊

--- api/views/auth_views.py
+++ api/views/auth_views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from api.models import Account
+from api.models import Account,Rank
 from utils.decorators import user_login_required
 
 
@@ -28,3 +28,18 @@ def login(request):
 def logout(request):
     request.session.flush()
     return Response({'success': True, 'message': '登出成功'})
+
+
+@api_view(['POST'])
+def register(request):
+    data = request.data
+
+    try:
+        btn_value = Rank.objects.get(pk=data['btn_value'])
+
+        Account.objects.create(pk=data['account'], password=data['password'], rank=btn_value)
+    except:
+        return Response({'success': False,'message': '註冊失敗'},status=status.HTTP_404_NOT_FOUND)
+
+    # Account.objects.get(pk=data['id'], pwd=data['pwd'])
+    return Response({'success': True, 'message': '註冊成功'})
