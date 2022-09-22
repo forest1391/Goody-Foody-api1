735a0936288801ad5fafb1698749405b9ecf5312
annie <10756031>
2022-09-22 16:19:24 +0800

    add_chat_done

--- api/urls/chat_urls.py
+++ api/urls/chat_urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 
 from api.views.tag_views import *
 
-from api.views.chat_views import get_all_chats
+from api.views.chat_views import get_all_chats, add_chat
 
 app_name = 'chat'
 
 urlpatterns = [
     path('all/', get_all_chats),
-    # path('add/', add_tag),
+    path('add/', add_chat),
 
     # path('get/<int:pk>/', get_review),
     # path('get_critic_reviews/', get_critic_reviews),
--- api/views/chat_views.py
+++ api/views/chat_views.py
@@ -28,13 +28,13 @@ def get_all_chats(request):
 
     })
 
-# @api_view(['POST'])
-# def add_tag(request):
-#     data = request.data
-#     try:
-#         Tags.objects.create(tag_id=data['tag_id'],tag_name=data['tag_name'],tag_type_id=data['tag_type_id'])
-#
-#     except:
-#         return Response({'success':False, "message":'新增失敗'}, status=status.HTTP_400_BAD_REQUEST)
-#
-#     return Response({'success':True, 'message':'新增成功'})
+@api_view(['POST'])
+def add_chat(request):
+    data = request.data
+    try:
+        Chat.objects.create(chat_id=data['chat_id'],account=data['account'],b_account=data['b_account'],content=data['content'],time=data['time'])
+
+    except:
+        return Response({'success':False, "message":'新增失敗'}, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response({'success':True, 'message':'新增成功'})
